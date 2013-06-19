2a56f52511bb3848e697f334b533a581fad58e4f
Juan Luis Rodríguez Ponce <jlrodriguez@emergya.com>
2013-06-19 19:12:58 +0200

    [#84172] Web services client Configure pom.xml to generate java from WSDL. Add sir-base-core as dependency.

--- src/main/java/com/emergya/siradmin/invest/App.java
+++ src/main/java/com/emergya/siradmin/invest/App.java
@@ -1,5 +1,8 @@
 package com.emergya.siradmin.invest;
 
+
+
+
 /**
  * Hello world!
  *
@@ -9,5 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+ 
+
+
     }
 }
--- src/test/java/com/emergya/siradmin/invest/AppTest.java
+++ src/test/java/com/emergya/siradmin/invest/AppTest.java
@@ -1,38 +1,87 @@
 package com.emergya.siradmin.invest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.List;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import javax.annotation.Resource;
+import javax.validation.constraints.AssertTrue;
+import javax.xml.namespace.QName;
+import javax.xml.rpc.ServiceException;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.emergya.ohiggins.model.ChileindicaInversionDataEntity;
+import com.emergya.siradmin.invest.client.keyquery.ConsultaLlavesResponse;
+import com.emergya.siradmin.invest.client.keyquery.DatosLlamada;
+import com.emergya.siradmin.invest.client.keyquery.LlavesInversionData;
+import com.emergya.siradmin.invest.client.keyquery.Respuesta;
+import com.emergya.siradmin.invest.client.keyquery.WSConsultaLlaves;
+import com.emergya.siradmin.invest.client.keyquery.WSConsultaLlavesLocator;
+import com.emergya.siradmin.invest.client.keyquery.WSConsultaLlavesPortType;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
+@Transactional
+public class AppTest {
+	private static final Log LOGGER = LogFactory.getLog(AppTest.class);
+
+	WSConsultaLlaves service;
+	@Resource
+	private SessionFactory sessionFactory;
+
+	@Before
+	public void setUp() throws Exception {
+		service = new WSConsultaLlavesLocator(
+				"http://qaweb.chileindica.cl/ws/consultallaves.php?wsdl",
+				QName.valueOf("{urn:WSConsultaLlaves}WSConsultaLlaves"));
+	}
+
+	@Test()
+	public void test() throws MalformedURLException, ServiceException,
+			RemoteException {
+		List<ChileindicaInversionDataEntity> result = sessionFactory.getCurrentSession().getNamedQuery("ChileindicaInversionDataEntity.findAll").list();
+		assertTrue(result.size() == 0);
+		ChileindicaInversionDataEntity nueva = new ChileindicaInversionDataEntity();
+		Long nuevaId = (Long) sessionFactory.getCurrentSession().save(nueva);
+		assertNotNull(nuevaId);
+		
+		WSConsultaLlavesPortType port = service
+				.getWSConsultaLlavesPort(new URL(
+						"http://qaweb.chileindica.cl/ws/consultallaves.php"));
+		ConsultaLlavesResponse response = port.WSConsultaLlaves(
+				BigInteger.valueOf(2013), BigInteger.valueOf(15));
+		Respuesta respuesta = response.getRespuesta();
+		assertNotNull(respuesta.getCodigoRespuesta());
+		assertEquals(BigInteger.ZERO, respuesta.getCodigoRespuesta());
+		assertEquals("Respuesta OK.", respuesta.getTextoRespuesta());
+		DatosLlamada datosLlamada = response.getDatosLlamada();
+		assertEquals(BigInteger.valueOf(2013), datosLlamada.getAno());
+		assertEquals(BigInteger.valueOf(15), datosLlamada.getRegion());
+		LlavesInversionData[] llavesInversion = response.getLlavesInversion();
+		for (LlavesInversionData llave : llavesInversion) {
+			LOGGER.debug("Año = " + llave.getAno() + "; C_Institucion = "
+					+ llave.getC_Institucion() + "; C_Preinversion = "
+					+ llave.getC_Preinversion() + "; C_Ficha = "
+					+ llave.getC_Ficha() + "; Fecha_Registro = "
+					+ llave.getFechaRegistro());
+		}
+
+	}
+
 }
