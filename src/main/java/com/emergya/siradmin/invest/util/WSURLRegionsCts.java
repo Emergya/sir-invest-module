726aaed0978f857ee2dd23548733ce024b192fce
Francisco Jesús Bernal Angulo <jbernal@emergya.com>
2015-05-14 13:02:16 +0200

    114895 Servicio de recuperación-actualización de iniciativas de inversión y controladores Spring MVC

---
+++ src/main/java/com/emergya/siradmin/invest/util/WSURLRegionsCts.java
@@ -0,0 +1,17 @@
+package com.emergya.siradmin.invest.util;
+
+public enum WSURLRegionsCts {
+    ARICA("arica"), OHIGGINS("ohiggins"), TARAPACA("tarapaca"), ANTOFAGASTA("antofagasta"), ATACAMA("atacama"), COQUIMBO("coquimbo"), VALPARAISO("valparaiso"), MAULE(
+            "maule"), BIOBIO("biobio"), ARAUCANIA("araucania"), LOS_RIOS("losrios"), LOS_LAGOS("loslagos"), AYSEN("aysen"), MAGALLANES("magallanes"), SANTIAGO(
+            "rms");
+
+    private String value;
+
+    WSURLRegionsCts(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
