a4acbc1211729b840ef9400f9b9d8090d1351b17
igutierrez <igutierrez@emergya.com>
2015-11-10 13:08:29 +0100

    Improve inversion import process

--- src/main/java/com/emergya/siradmin/invest/util/WSURLRegionsCts.java
+++ src/main/java/com/emergya/siradmin/invest/util/WSURLRegionsCts.java
@@ -1,17 +1,19 @@
 package com.emergya.siradmin.invest.util;
 
 public enum WSURLRegionsCts {
-   ARICA("arica"), OHIGGINS("ohiggins"), TARAPACA("tarapaca"), ANTOFAGASTA("antofagasta"), ATACAMA("atacama"), COQUIMBO("coquimbo"), VALPARAISO("valparaiso"), MAULE(
-            "maule"), BIOBIO("biobio"), ARAUCANIA("araucania"), LOS_RIOS("losrios"), LOS_LAGOS("loslagos"), AYSEN("aysen"), MAGALLANES("magallanes"), SANTIAGO(
-            "rms");
+	ARICA("arica"), OHIGGINS("ohiggins"), TARAPACA("tarapaca"), ANTOFAGASTA(
+			"antofagasta"), ATACAMA("atacama"), COQUIMBO("coquimbo"), VALPARAISO(
+			"valparaiso"), MAULE("maule"), BIOBIO("biobio"), ARAUCANIA(
+			"araucania"), LOS_RIOS("losrios"), LOS_LAGOS("loslagos"), AYSEN(
+			"aysen"), MAGALLANES("magallanes"), SANTIAGO("rms");
 
-    private String value;
+	private String value;
 
-    WSURLRegionsCts(String value) {
-        this.value = value;
-    }
+	WSURLRegionsCts(String value) {
+		this.value = value;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 }
